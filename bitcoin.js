3c3b5a54fb632c5b1c027a8d68687de1199fca96
danielgaban <91639510+danielgaban@users.noreply.github.com>
2023-11-29 10:10:58 -0400

    feat(bytes-table): added 3 bytes calculation on a table

--- bitcoin.js
+++ bitcoin.js
@@ -1,3 +1,33 @@
+function handlerChangeText(newText, element) {
+  element.textContent = newText;
+}
+
+function createTableRows(regularPrice, usdPrice) {
+  const customBytes = [125, 200, 500] 
+  const attributes = ['bytes', 'sat', 'dolar']
+  const table = document.querySelector('table')
+  const body = table?.querySelector('tbody')
+  customBytes.forEach((bytes) => {
+    const row = document.createElement('tr')
+    attributes.forEach((attribute, i) => {
+      const element = document.createElement('td')
+      switch (attribute) {
+        case 'bytes':
+          element.textContent = bytes.toString();
+          break
+        case 'sat':
+          element.textContent = (regularPrice * bytes).toString();
+          break
+        case 'dolar':
+          element.textContent = "$ "+(regularPrice * bytes * usdPrice / 100000000).toFixed(2)
+          break
+      }
+      row.appendChild(element)
+    })
+    body?.appendChild(row)
+  });
+}
+
 async function handleRequest() {
     try {
       const {fee} = await chrome.storage.local.get('fee')
@@ -5,10 +35,9 @@ async function handleRequest() {
       if (err) throw new Error('failed to get storage')
       const res = await fetch('https://blockchain.info/ticker')
       const { USD } = await res.json()
-      document.querySelector('#regularPrice').textContent = regular
-      document.querySelector('#priorityPrice').textContent = priority
-      document.querySelector('#transactionPrice').textContent = regular * 150
-      document.querySelector('#transactionDolar').textContent = (regular * 150 * USD.last / 100000000).toFixed(2)
+      handlerChangeText(regular, document.querySelector('#regularPrice'))
+      handlerChangeText(priority, document.querySelector('#priorityPrice'))
+      createTableRows(regular, USD.last)
       console.log(regular, priority)
     } catch (err) {
       console.log(err)
